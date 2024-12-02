4231d6a858e840b06cf8ae7c280cd85b71c93a15
EliPeixoto <eliane.peixoto@outlook.com>
2024-12-02 20:00:24 -0300

    migration

---
+++ src/main/resources/db/migration/V2__produto_schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE produto (
+                         id BIGINT PRIMARY KEY,
+                         nome VARCHAR(255) NOT NULL,
+                         descricao VARCHAR(255),
+                         quantidade INT NOT NULL
+);
